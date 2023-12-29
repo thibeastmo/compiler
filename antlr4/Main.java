6389d3a887afd39eebfe2952c30891722c5b118d
thikdgmo <thimo.mortelmans@student.kdg.be>
2023-12-29 23:45:34 +0100

    Compilede java uitvoerbaar

--- antlr4/Main.java
+++ antlr4/Main.java
@@ -1,26 +1,17 @@
-import org.antlr.v4.runtime.*;
-import org.antlr.v4.runtime.tree.*;
-
 public class Main {
-    public static void main(String[] args) throws Exception {
-        // Create an input stream from a file or string
-        CharStream input = CharStreams.fromFileName("Input.java");
+    private int attribute;
 
-        // Create a lexer
-        BasicJavaLexer lexer = new BasicJavaLexer(input);
+    public Main(int initialValue) {
+        this.attribute = initialValue;
+    }
 
-        // Create a token stream from the lexer
-        CommonTokenStream tokens = new CommonTokenStream(lexer);
+    public int sum(int x) {
+        return x + attribute;
+    }
 
-        // Create a parser
-        BasicJavaParser parser = new BasicJavaParser(tokens);
-
-        // Parse the input and get the parse tree
-        ParseTree tree = parser.compilationUnit();
-
-        // Optionally, use a listener or visitor to process the parse tree
-        MyListener listener = new MyListener();
-        ParseTreeWalker walker = new ParseTreeWalker();
-        walker.walk(listener, tree);
+    public static void main(String[] args) {
+        Main instance = new Main(5);
+        int result = instance.sum(4);
+        System.out.println(result);
     }
 }
