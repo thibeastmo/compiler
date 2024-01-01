efdb027a9be24f988e31b630f27f5e825909c61d
thikdgmo <thimo.mortelmans@student.kdg.be>
2024-01-01 19:09:43 +0100

    Declareren van variable in visitor

--- compiler/AntlrToExpression.java
+++ compiler/AntlrToExpression.java
@@ -1,11 +1,42 @@
+import BasicJavaBaseVisitor;
+import ;
+
 public class AntlrToExpression extends BasicJavaBaseVisitor<Expression> {
+    private List<String> vars; //stores all the variables declared in the program so far
+    private List<String> semanticErrors;
+
 	@Override 
     public T visitDeclarating(BasicJavaParser.DeclaratingContext ctx) {
         return visitChildren(ctx);
     }
 	@Override 
     public T visitDeclaration(BasicJavaParser.DeclarationContext ctx) {
-        return visitChildren(ctx);
+        //ID() is a method generated to correspond to the token ID in the source grammar.
+        Token idToken = ctx.ID().getSymbol(); // same as ctx.getChild(1).getSymbol()
+        int line = idToken.getLine();
+        int column = idToken.getCharPositionLine() + 1; //0 indexed +1
+        String id = ctx.getChild(1);
+
+        //Mainting vars list for semantic error reporting
+        if (vars.contains(id)) {
+            semanticErrors.add("Error: variable \"" + id + "\" already declared (line: " + line + ", column: " + column + ")");
+        }
+        else {
+            vars.add(id);
+        }
+        String type = ctx.getChild(0).getText();
+        String valueText = ctx.getChild(3).getText();
+        if (type.equals("text")) {
+            return new VariableDeclaration(id, type, valueText);
+        }
+        else if (type.equals("int")) {
+            int value = Integer.parseInt(valueText);
+            return new VariableDeclaration(id, type, value);
+        }
+        else {
+            //boolean
+            return new VariableDeclaration(id, type, valueText.equals("true"))
+        }
     }
     @Override 
     public T visitMethodDeclaration(BasicJavaParser.MethodDeclarationContext ctx) {
@@ -53,17 +84,17 @@ public class AntlrToExpression extends BasicJavaBaseVisitor<Expression> {
     }
 	@Override 
     public T visitNumber(BasicJavaParser.NumberContext ctx) {
-        String numText = ctx.getChild(0).getText();
+        String numText = ctx.getChild(1).getText();
         int num = Integer.parseInt(numText);
         return new Number(num);
     }
 	@Override 
     public T visitBool(BasicJavaParser.BoolContext ctx) { 
-        String boolText = ctx.getChild(0).getText();
+        String boolText = ctx.getChild(1).getText();
         return boolText.equals("true") ? true : false;
     }
 	@Override
     public T visitText(BasicJavaParser.TextContext ctx) { 
-        return ctx.getChild(0).getText();
+        return ctx.getChild(1).getText();
     }
 }
